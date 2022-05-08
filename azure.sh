a614fe26b3eec990f05bb42fd004e55493f6a65f
David de Mingo <david@optersoft.com>
2022-05-08 18:14:38 +0200

    add prod command

--- azure.sh
+++ azure.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-case $1 in 
+case $1 in
     login)
+        az login --tenant ddemingolamerce.onmicrosoft.com
+        az configure --defaults group='test'
+    ;;
+    login-sandbox)
         az login --tenant learn.docs.microsoft.com
         az configure --defaults group=`az group list --query '[].name' --output tsv`
     ;;
@@ -9,9 +13,17 @@ case $1 in
         # https://docs.microsoft.com/en-us/cli/azure/deployment/group?view=azure-cli-latest
         az deployment group create --template-file $2
     ;;
-    deploy-complete)
-        az deployment group create --template-file $2 --mode Complete
+    group-create)
+        az group create --location francecentral --name test  
     ;;
+    group-delete)
+        az group delete --location francecentral --name test
+    ;;
+    *)
+        echo "unkown command"
+    ;;
+    #az bicep decompile --file azuredeploy.json
+
 esac
 
 # https://docs.microsoft.com/es-es/learn/modules/improve-app-scalability-resiliency-with-load-balancer/4-exercise-configure-public-load-balancer?pivots=bash
